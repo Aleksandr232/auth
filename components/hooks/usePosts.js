96e42e2568665c97ebedf7135af9fc8a50ffe602
Aleksandr232 <aleksrowing232@gmail.com>
2022-07-15 21:51:10 +0300

    app

--- components/hooks/usePosts.js
+++ components/hooks/usePosts.js
@@ -1,1 +1,20 @@
-import React,{useState, UseEffect} from "react";
+import {useState, UseEffect} from "react";
+import {POST} from "../POST";
+
+const LIMIT = 2
+
+export default function usePosts(){
+    const [isLoading, setLoading] = useState(true)
+    const [posts, setPosts] = useState([])
+
+
+    UseEffect((id)=>{
+        POST(0, LIMIT).then(newPosts=>{
+            setPosts(newPosts);
+            setLoading(flase)
+        })
+    },[])
+
+    return {isLoading, posts};
+}
+
